576e1c29e9ef54729b7c6c410aa1748fb96cba75
Kaewmanee <64160234@go.buu.ac.th>
2024-07-16 13:04:54 +0700

    chore: Update npm dependencies and add dotenv configuration for local environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,21 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
 import { BooksModule } from './books/books.module';
 import { Book } from './books/entities/book.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.local'],
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'rootpassword',
-      database: 'library_db',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: [Book],
       synchronize: true,
     }),
